5b4a646424c201f76cbd5c397470c0f8a98dcb19
Dusan Fister <3198785+rhododendrom@users.noreply.github.com>
2024-06-13 14:27:29 +0200

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -48,21 +48,8 @@ class AE_PRED:
         input_img = keras.Input(shape=(self.input_dim, self.input_dim, self.channels_in))
         x = random_mask(input_img)
 
-        # x = layers.Conv2D(32, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(32, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(32, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-
-        # x = layers.Conv2D(32, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(32, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
         x = layers.Conv2D(64, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=3)(x)
-
-        # x = layers.Conv2D(64, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(64, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
         x = layers.Conv2D(128, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=3)(x)
-
-        # x = layers.Conv2D(128, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(128, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(256, 3, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=3)(x)
 
         x = layers.Flatten()(x)
         x = layers.Dense(256, activation=layers.LeakyReLU(alpha=0.3))(x)
@@ -86,8 +73,6 @@ class AE_PRED:
 
         x = layers.Conv2DTranspose(128, 4, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=3)(x)
         x = layers.Conv2DTranspose(64, 4, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=3)(x)
-        # x = layers.Conv2DTranspose(32, 4, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
-        # x = layers.Conv2D(3, 4, activation=layers.LeakyReLU(alpha=0.3), padding='same', strides=1)(x)
 
         decoded = layers.Conv2D(self.channels_out, 2, activation='selu', padding='valid', strides=1)(x)
 
